d6f23a90f58e4e08efb517c25fd4bfc8aea3bc32
EC2 Default User <ec2-user@ip-172-31-39-175.eu-north-1.compute.internal>
2024-08-06 08:00:03 +0000

    files are modified and added

--- 1.sh
+++ 1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-default_username="admin"
-default_password="Admin123"
-
-echo "Enter username:"
-read username
-echo "Enter password:"
-read -s password
-
-if [ "$username" == "$default_username" ] && [ "$password" == "$default_password" ]; then
-    echo "Authentication successful."
+user1="admin"
+pass1="Admin123"
+user2="developer"
+pass2="JavaPython@123"
+read -p "Username: " username
+read -s -p "Password: " password
+echo
+if [[ "$username" == "$user1" && "$password" == "$pass1" ]] || [[ "$username" == "$user2" && "$password" == "$pass2" ]]; then
+    echo "Authentication successful!"
 else
     echo "Authentication failed."
-fi 
+fi
+ 
