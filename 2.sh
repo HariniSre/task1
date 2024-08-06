2acbcb8ebe95ef3c9b9318d18675da805e5bf2d1
EC2 Default User <ec2-user@ip-172-31-39-175.eu-north-1.compute.internal>
2024-08-06 08:07:11 +0000

    files are modified and added

--- 2.sh
+++ 2.sh
@@ -4,7 +4,10 @@ if ((age < 13)); then
     echo "You are a kid."
 elif ((age >= 13 && age <= 19)); then
     echo "You are a teenager."
-else
+elif ((age >= 20 && age < 60)); then
     echo "You are an adult."
+else
+    echo "You are in the old age group."
 fi
+
 
